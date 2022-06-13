d9b0ef2752463ae09e9e078467f8008338ecf2a2
Grace Njeri <gracenjeri.gnw@gmail.com>
2022-06-13 22:54:03 +0300

    101-make_me_win

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/win.so https://github.com/devgracie/alx-low_level_programming/blob/master/0x18-dynamic_libraries/wins.so
-export LD_PRELOAD=/tmp/wins.so
+wget -P /tmp/ https://raw.github.com/luischaparroc/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
+export LD_PRELOAD=/tmp/nrandom.so
